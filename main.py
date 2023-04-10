7ee9fb660b7f61c3d0066de1a00eae597b96cd5a
codeRookieErick <erickfernandomoraramirez@gmail.com>
2023-04-10 07:59:07 -0400

    Adding version and license to files. Updating MANIFEST.in

--- main.py
+++ main.py
@@ -1,15 +1,5 @@
-#import os
-#from backupbot.tools.directory import get_files
-#from backupbot.tools.cryptography import get_file_hash
-#from backupbot.tools.compression import extract_to_folder, update
-
 import time
-from src.chelydra.core import create_version, restore_into
-
-#update('project.zip', 'testarazo/prr.txt', 'main.py')
-#extract_to_folder('project.zip', 'testarazo/prr.txt', 'output')
-#
-#print([get_file_hash(i) for i in get_files('.')])
+from src.chelydra.backup import create_version, restore_into
 
 source = r"D:\test\chelydra\source"
 backup = r"D:\test\chelydra\backup"
