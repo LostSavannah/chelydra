b52b73a37a3d341a46e8c8c8327d58ec52638f37
Erick Fernando Mora Ramirez <erickmoradev@gmail.com>
2024-11-24 13:55:23 -0400

    Update test_success.py

--- tests/test_success.py
+++ tests/test_success.py
@@ -1,9 +1,3 @@
-from chelydra import backup
-
-BACKUP_FOLDER = "./backup"
-SOURCE_FOLDER = "./source_folder"
-TARGET_FOLDER = "./target_folder"
-
 # Not a real test, just setting this dummy test for validatinf pytest
 def test_success():
     assert 1 == 1
